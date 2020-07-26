e1eac5a4042a8c83c9f14037b7d7de74a462f733
superDongE <dlagoehd13@gmail.com>
2020-07-26 15:50:42 +0900

    20200726

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,43 +1,43 @@
-const SET_DIFF = 'counter/SET_DIFF';
-const INCREASE = 'counter/INCREASE';
-const DECREASE = 'counter/DECREASE';
+const SET_DIFF = "counter/SET_DIFF";
+const INCREASE = "counter/INCREASE";
+const DECREASE = "counter/DECREASE";
 
-export const setDpff = diff => ({
-    type: 'SET_DIFF',
-    diff
-})
+export const setDiff = (diff) => ({
+  type: "SET_DIFF",
+  diff,
+});
 
 export const increase = () => ({
-    type: 'INCREASE'
+  type: "INCREASE",
 });
 
 export const decrease = () => ({
-    type: 'DECREASE'
+  type: "DECREASE",
 });
 
 const initialState = {
-    number: 0,
-    diff : 1
+  number: 0,
+  diff: 1,
 };
 
-export default function counter(state= initialState, action) {
-    switch(action.type) {
-        case SET_DIFF:
-            return {
-                ...state,
-                diff: action.diff
-            };
-        case INCREASE:
-            return {
-                ...state,
-                number: state.number + state.diff
-            };
-        case INCREASE:
-            return {
-               ...state,
-               number: state.number - state.diff
-            };
-        default: 
-        return state;
-    }
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case SET_DIFF:
+      return {
+        ...state,
+        diff: action.diff,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        number: state.number + state.diff,
+      };
+    case INCREASE:
+      return {
+        ...state,
+        number: state.number - state.diff,
+      };
+    default:
+      return state;
+  }
 }
